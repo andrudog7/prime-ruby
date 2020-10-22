da646bc78f298b2fb4deabd7e869313519019f79
--add <andrewksmoker@gmail.com>
2020-10-22 10:16:27 -0400

    commit tree 1ed9b36c75222685d54ae144d36cad4ccab6fd15

--- prime.rb
+++ prime.rb
@@ -1,1 +1,28 @@
-# Add  code here!
+require 'benchmark'
+
+# puts Benchmark.measure {
+# def prime?(number)
+#     if number <= 1
+#         return false
+#     else
+#     (2..(number-1)).each do |n|
+#         return false if number % n == 0 
+#         end
+#         true
+# end 
+# end
+# }
+
+puts Benchmark.measure {
+    def prime?(number)
+        n = 2
+        if n > number 
+        false
+        else while n < number
+            return false if number % n == 0
+            n+=1
+            end
+        true
+        end
+    end
+}
